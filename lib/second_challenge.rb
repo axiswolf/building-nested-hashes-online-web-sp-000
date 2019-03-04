e5324e4b45ada19e3d0ede56bc461ba2c7da37d0
Karen V <karenvo@usc.edu>
2019-03-04 10:11:15 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,7 +6,7 @@ def second_challenge
       :matriarch => {}
       :heroine => {}
       :hero_friends => []
-      } 
+      }
     :capulet => {
       #your key/value pairs here!
       :patriarch => {}
@@ -15,5 +15,5 @@ def second_challenge
       :hero_friends => []
       }
 
-  
+
 end
